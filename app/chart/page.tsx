5697ade7361c5a012043e4ac4339fd307d2ab575
ShadmanShakib <sm.shakib007@gmail.com>
2023-11-05 13:11:27 +0600

    update

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -3,12 +3,6 @@ import ChartFour from "@/components/Charts/ChartFour";
 import ChartOne from "@/components/Charts/ChartOne";
 import ChartThree from "@/components/Charts/ChartThree";
 import ChartTwo from "@/components/Charts/ChartTwo";
-import { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "Chart Page | Next.js E-commerce Dashboard Template",
-  description: "This is Chart Page for TailAdmin Next.js",
-  // other metadata
-};
 
 const Chart = () => {
   return (
