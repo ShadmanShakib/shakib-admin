1dd7186cd27a5753f7e23faaa5f810e1565f7172
ShadmanShakib <sm.shakib007@gmail.com>
2023-11-13 20:40:28 +0600

    charts

--- components/Dashboard/E-commerce.tsx
+++ components/Dashboard/E-commerce.tsx
@@ -1,8 +1,7 @@
 "use client";
 import React from "react";
 import ChartOne from "../Charts/ChartOne";
-import ChartThree from "../Charts/ChartThree";
-import ChartTwo from "../Charts/ChartTwo";
+import { AreaChart, SimpleBar } from "@/components/Charts";
 import ChatCard from "../Chat/ChatCard";
 import TableOne from "../Tables/TableOne";
 import CardDataStats from "../CardDataStats";
@@ -10,6 +9,7 @@ import CardDataStats from "../CardDataStats";
 
 // without this the component renders on server and throws an error
 import dynamic from "next/dynamic";
+import DataCard from "../Cards/DataCard";
 const MapOne = dynamic(() => import("../Maps/MapOne"), {
   ssr: false,
 });
@@ -17,27 +17,8 @@ const MapOne = dynamic(() => import("../Maps/MapOne"), {
 const ECommerce: React.FC = () => {
   return (
     <>
-
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardDataStats title="Total views" total="$3.456K" rate="0.43%" levelUp>
-          <svg
-            className="fill-primary dark:fill-white"
-            width="22"
-            height="16"
-            viewBox="0 0 22 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M11 15.1156C4.19376 15.1156 0.825012 8.61876 0.687512 8.34376C0.584387 8.13751 0.584387 7.86251 0.687512 7.65626C0.825012 7.38126 4.19376 0.918762 11 0.918762C17.8063 0.918762 21.175 7.38126 21.3125 7.65626C21.4156 7.86251 21.4156 8.13751 21.3125 8.34376C21.175 8.61876 17.8063 15.1156 11 15.1156ZM2.26876 8.00001C3.02501 9.27189 5.98126 13.5688 11 13.5688C16.0188 13.5688 18.975 9.27189 19.7313 8.00001C18.975 6.72814 16.0188 2.43126 11 2.43126C5.98126 2.43126 3.02501 6.72814 2.26876 8.00001Z"
-              fill=""
-            />
-            <path
-              d="M11 10.9219C9.38438 10.9219 8.07812 9.61562 8.07812 8C8.07812 6.38438 9.38438 5.07812 11 5.07812C12.6156 5.07812 13.9219 6.38438 13.9219 8C13.9219 9.61562 12.6156 10.9219 11 10.9219ZM11 6.625C10.2437 6.625 9.625 7.24375 9.625 8C9.625 8.75625 10.2437 9.375 11 9.375C11.7563 9.375 12.375 8.75625 12.375 8C12.375 7.24375 11.7563 6.625 11 6.625Z"
-              fill=""
-            />
-          </svg>
-        </CardDataStats>
+        <DataCard />
         <CardDataStats title="Total Profit" total="$45,2K" rate="4.35%" levelUp>
           <svg
             className="fill-primary dark:fill-white"
@@ -104,11 +85,12 @@ const ECommerce: React.FC = () => {
           </svg>
         </CardDataStats>
       </div>
+      <div className="space-y-5 py-5">
+        <AreaChart />
+        <SimpleBar />
+      </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
-        <ChartThree />
         <MapOne />
         <div className="col-span-12 xl:col-span-8">
           <TableOne />
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,49 +6,51 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}", // Tremor module
   ],
   darkMode: "class",
   theme: {
+    transparent: "transparent",
     fontFamily: {
       satoshi: ["Satoshi", "sans-serif"],
     },
-    colors: {
-      current: "currentColor",
-      transparent: "transparent",
-      white: "#FFFFFF",
-      black: "#1C2434",
-      "black-2": "#010101",
-      body: "#64748B",
-      bodydark: "#AEB7C0",
-      bodydark1: "#DEE4EE",
-      bodydark2: "#8A99AF",
-      primary: "#3C50E0",
-      secondary: "#80CAEE",
-      stroke: "#E2E8F0",
-      gray: "#EFF4FB",
-      graydark: "#333A48",
-      "gray-2": "#F7F9FC",
-      "gray-3": "#FAFAFA",
-      whiten: "#F1F5F9",
-      whiter: "#F5F7FD",
-      boxdark: "#24303F",
-      "boxdark-2": "#1A222C",
-      strokedark: "#2E3A47",
-      "form-strokedark": "#3d4d60",
-      "form-input": "#1d2a39",
-      "meta-1": "#DC3545",
-      "meta-2": "#EFF2F7",
-      "meta-3": "#10B981",
-      "meta-4": "#313D4A",
-      "meta-5": "#259AE6",
-      "meta-6": "#FFBA00",
-      "meta-7": "#FF6766",
-      "meta-8": "#F0950C",
-      "meta-9": "#E5E7EB",
-      success: "#219653",
-      danger: "#D34053",
-      warning: "#FFA70B",
-    },
+    // colors: {
+    //   current: "currentColor",
+    //   transparent: "transparent",
+    //   white: "#FFFFFF",
+    //   black: "#1C2434",
+    //   "black-2": "#010101",
+    //   body: "#64748B",
+    //   bodydark: "#AEB7C0",
+    //   bodydark1: "#DEE4EE",
+    //   bodydark2: "#8A99AF",
+    //   primary: "#3C50E0",
+    //   secondary: "#80CAEE",
+    //   stroke: "#E2E8F0",
+    //   gray: "#EFF4FB",
+    //   graydark: "#333A48",
+    //   "gray-2": "#F7F9FC",
+    //   "gray-3": "#FAFAFA",
+    //   whiten: "#F1F5F9",
+    //   whiter: "#F5F7FD",
+    //   boxdark: "#24303F",
+    //   "boxdark-2": "#1A222C",
+    //   strokedark: "#2E3A47",
+    //   "form-strokedark": "#3d4d60",
+    //   "form-input": "#1d2a39",
+    //   "meta-1": "#DC3545",
+    //   "meta-2": "#EFF2F7",
+    //   "meta-3": "#10B981",
+    //   "meta-4": "#313D4A",
+    //   "meta-5": "#259AE6",
+    //   "meta-6": "#FFBA00",
+    //   "meta-7": "#FF6766",
+    //   "meta-8": "#F0950C",
+    //   "meta-9": "#E5E7EB",
+    //   success: "#219653",
+    //   danger: "#D34053",
+    //   warning: "#FFA70B",
+    // },
     screens: {
       "2xsm": "375px",
       xsm: "425px",
@@ -56,6 +58,105 @@ module.exports = {
       ...defaultTheme.screens,
     },
     extend: {
+      colors: {
+        //
+        current: "currentColor",
+        transparent: "transparent",
+        white: "#FFFFFF",
+        black: "#1C2434",
+        "black-2": "#010101",
+        body: "#64748B",
+        bodydark: "#AEB7C0",
+        bodydark1: "#DEE4EE",
+        bodydark2: "#8A99AF",
+        primary: "#3C50E0",
+        secondary: "#80CAEE",
+        stroke: "#E2E8F0",
+        gray: "#EFF4FB",
+        graydark: "#333A48",
+        "gray-2": "#F7F9FC",
+        "gray-3": "#FAFAFA",
+        whiten: "#F1F5F9",
+        whiter: "#F5F7FD",
+        boxdark: "#24303F",
+        "boxdark-2": "#1A222C",
+        strokedark: "#2E3A47",
+        "form-strokedark": "#3d4d60",
+        "form-input": "#1d2a39",
+        "meta-1": "#DC3545",
+        "meta-2": "#EFF2F7",
+        "meta-3": "#10B981",
+        "meta-4": "#313D4A",
+        "meta-5": "#259AE6",
+        "meta-6": "#FFBA00",
+        "meta-7": "#FF6766",
+        "meta-8": "#F0950C",
+        "meta-9": "#E5E7EB",
+        success: "#219653",
+        danger: "#D34053",
+        warning: "#FFA70B",
+        // light mode
+        tremor: {
+          brand: {
+            faint: "#eff6ff", // blue-50
+            muted: "#bfdbfe", // blue-200
+            subtle: "#60a5fa", // blue-400
+            DEFAULT: "#3b82f6", // blue-500
+            emphasis: "#1d4ed8", // blue-700
+            inverted: "#ffffff", // white
+          },
+          background: {
+            muted: "#f9fafb", // gray-50
+            subtle: "#f3f4f6", // gray-100
+            DEFAULT: "#ffffff", // white
+            emphasis: "#374151", // gray-700
+          },
+          border: {
+            DEFAULT: "#e5e7eb", // gray-200
+          },
+          ring: {
+            DEFAULT: "#e5e7eb", // gray-200
+          },
+          content: {
+            subtle: "#9ca3af", // gray-400
+            DEFAULT: "#6b7280", // gray-500
+            emphasis: "#374151", // gray-700
+            strong: "#111827", // gray-900
+            inverted: "#ffffff", // white
+          },
+        },
+        // dark mode
+        "dark-tremor": {
+          brand: {
+            faint: "#0B1229", // custom
+            muted: "#172554", // blue-950
+            subtle: "#1e40af", // blue-800
+            DEFAULT: "#3b82f6", // blue-500
+            emphasis: "#60a5fa", // blue-400
+            inverted: "#030712", // gray-950
+          },
+          background: {
+            muted: "#131A2B", // custom
+            subtle: "#1f2937", // gray-800
+            DEFAULT: "#111827", // gray-900
+            emphasis: "#d1d5db", // gray-300
+          },
+          border: {
+            DEFAULT: "#1f2937", // gray-800
+          },
+          ring: {
+            DEFAULT: "#1f2937", // gray-800
+          },
+          content: {
+            subtle: "#4b5563", // gray-600
+            DEFAULT: "#6b7280", // gray-500
+            emphasis: "#e5e7eb", // gray-200
+            strong: "#f9fafb", // gray-50
+            inverted: "#000000", // black
+          },
+        },
+      },
+
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -70,6 +171,11 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      borderRadius: {
+        "tremor-small": "0.375rem",
+        "tremor-default": "0.5rem",
+        "tremor-full": "9999px",
+      },
       fontSize: {
         "title-xxl": ["44px", "55px"],
         "title-xl": ["36px", "45px"],
@@ -79,6 +185,10 @@ module.exports = {
         "title-md2": ["26px", "30px"],
         "title-sm": ["20px", "26px"],
         "title-xsm": ["18px", "24px"],
+        "tremor-label": ["0.75rem"],
+        "tremor-default": ["0.875rem", { lineHeight: "1.25rem" }],
+        "tremor-title": ["1.125rem", { lineHeight: "1.75rem" }],
+        "tremor-metric": ["1.875rem", { lineHeight: "2.25rem" }],
       },
       spacing: {
         4.5: "1.125rem",
@@ -248,6 +358,18 @@ module.exports = {
         6: "0px 3px 15px rgba(0, 0, 0, 0.1)",
         7: "-5px 0 0 #313D4A, 5px 0 0 #313D4A",
         8: "1px 0 0 #313D4A, -1px 0 0 #313D4A, 0 1px 0 #313D4A, 0 -1px 0 #313D4A, 0 3px 13px rgb(0 0 0 / 8%)",
+        // light
+        "tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+        "tremor-card":
+          "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+        "tremor-dropdown":
+          "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+        // dark
+        "dark-tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+        "dark-tremor-card":
+          "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+        "dark-tremor-dropdown":
+          "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
       },
       dropShadow: {
         1: "0px 1px 0px #E2E8F0",
@@ -268,5 +390,34 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  safelist: [
+    {
+      pattern:
+        /^(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /^(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /^(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+      variants: ["hover", "ui-selected"],
+    },
+    {
+      pattern:
+        /^(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+    {
+      pattern:
+        /^(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+    {
+      pattern:
+        /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+    },
+  ],
+  plugins: [require("@headlessui/tailwindcss")],
 };
