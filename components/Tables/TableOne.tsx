65e11fbc49bb79d13fb600e445b6c1f68ecfc9cd
ShadmanShakib <sm.shakib007@gmail.com>
2023-12-11 22:37:13 +0600

    table update

--- components/Tables/TableOne.tsx
+++ components/Tables/TableOne.tsx
@@ -1,5 +1,14 @@
 import { BRAND } from "@/types/brand";
 import Image from "next/image";
+import {
+  Table,
+  TableHead,
+  TableHeaderCell,
+  TableBody,
+  TableRow,
+  TableCell,
+  Title,
+} from "@tremor/react";
 
 const brandData: BRAND[] = [
   {
@@ -46,76 +55,56 @@ const brandData: BRAND[] = [
 
 const TableOne = () => {
   return (
-    <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
-      <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        Top Channels
-      </h4>
+    <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <Title>Top Channels</Title>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeaderCell>Source</TableHeaderCell>
+            <TableHeaderCell>Visitors</TableHeaderCell>
+            <TableHeaderCell>Revenues</TableHeaderCell>
+            <TableHeaderCell>Sales</TableHeaderCell>
+            <TableHeaderCell>Conversion</TableHeaderCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {brandData.map((brand, key) => (
+            <TableRow
+              className={`grid grid-cols-3 sm:grid-cols-5 ${
+                key === brandData.length - 1
+                  ? ""
+                  : "border-b border-stroke dark:border-strokedark"
+              }`}
+              key={key}
+            >
+              <TableCell className="flex items-center gap-3 p-2.5 xl:p-5">
+                <div className="flex-shrink-0">
+                  <Image src={brand.logo} alt="Brand" width={48} height={48} />
+                </div>
+                <p className="hidden text-black dark:text-white sm:block">
+                  {brand.name}
+                </p>
+              </TableCell>
 
-      <div className="flex flex-col">
-        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
-          <div className="p-2.5 xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Source
-            </h5>
-          </div>
-          <div className="p-2.5 text-center xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Visitors
-            </h5>
-          </div>
-          <div className="p-2.5 text-center xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Revenues
-            </h5>
-          </div>
-          <div className="hidden p-2.5 text-center sm:block xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Sales
-            </h5>
-          </div>
-          <div className="hidden p-2.5 text-center sm:block xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Conversion
-            </h5>
-          </div>
-        </div>
+              <TableCell className="flex items-center justify-center p-2.5 xl:p-5">
+                <p className="text-black dark:text-white">{brand.visitors}K</p>
+              </TableCell>
 
-        {brandData.map((brand, key) => (
-          <div
-            className={`grid grid-cols-3 sm:grid-cols-5 ${
-              key === brandData.length - 1
-                ? ""
-                : "border-b border-stroke dark:border-strokedark"
-            }`}
-            key={key}
-          >
-            <div className="flex items-center gap-3 p-2.5 xl:p-5">
-              <div className="flex-shrink-0">
-                <Image src={brand.logo} alt="Brand" width={48} height={48} />
-              </div>
-              <p className="hidden text-black dark:text-white sm:block">
-                {brand.name}
-              </p>
-            </div>
+              <TableCell className="flex items-center justify-center p-2.5 xl:p-5">
+                <p className="text-meta-3">${brand.revenues}</p>
+              </TableCell>
 
-            <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-black dark:text-white">{brand.visitors}K</p>
-            </div>
+              <TableCell className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                <p className="text-black dark:text-white">{brand.sales}</p>
+              </TableCell>
 
-            <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-meta-3">${brand.revenues}</p>
-            </div>
-
-            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-black dark:text-white">{brand.sales}</p>
-            </div>
-
-            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-meta-5">{brand.conversion}%</p>
-            </div>
-          </div>
-        ))}
-      </div>
+              <TableCell className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                <p className="text-meta-5">{brand.conversion}%</p>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 };
