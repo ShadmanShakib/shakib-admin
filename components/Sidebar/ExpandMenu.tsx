d4de238164840947bae67122781fa341aea8276f
ShadmanShakib <sm.shakib007@gmail.com>
2024-01-25 22:05:14 +0600

    sidebar text color

--- components/Sidebar/ExpandMenu.tsx
+++ components/Sidebar/ExpandMenu.tsx
@@ -27,19 +27,24 @@ const ExpandMenu = (props: Props) => {
     <Collapsible open={open} onOpenChange={setOpen} className="">
       <CollapsibleTrigger asChild className="group rounded-sm px-3 py-2  ">
         {isSidebarOpen ? (
-          <div className="flex w-full items-center ">
+          <div className="flex w-full items-center text-gray-2 ">
             {icon}
             <div
               className={cn(
                 "flex w-full transform items-center justify-between duration-300 ease-in",
               )}
             >
-              <p className={cn("ml-2 text-lg group-hover:text-white ", {})}>
+              <p
+                className={cn(
+                  "ml-2 text-lg text-gray-2 group-hover:text-white ",
+                  {},
+                )}
+              >
                 {name}
               </p>
               <ChevronRight
                 className={cn(
-                  "ml-2 h-4 w-4 transform  duration-300 ease-in-out group-hover:rotate-90 ",
+                  "text-gary-2 ml-2 h-4 w-4  duration-300 ease-in-out group-hover:rotate-90 ",
                   {
                     "rotate-90": open,
                   },
